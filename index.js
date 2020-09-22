643fa66b280a17c9a4765160bc945b2957790f35
Christine Ross <christinejoyross@gmail.com>
2020-09-22 12:48:46 -0700

    first prompt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -17,3 +18,13 @@ var connection = mysql.createConnection({
     // run the start function after the connection is made to prompt the user
     start();
   });
+
+  function start(){
+      inquirer  
+        .prompt({
+            name: "FirstAction",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View All Roles", "Add Role", "Remove Role"]
+        })
+  }
