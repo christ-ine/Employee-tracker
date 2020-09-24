9a07aef3014d26536d3eaaedaa93053f5d68644c
Christine Ross <christinejoyross@gmail.com>
2020-09-24 16:39:26 -0700

    add employee prompts

--- index.js
+++ index.js
@@ -11,23 +11,23 @@ var connection = mysql.createConnection({
     // Your password
     password: "root",
     database: "employee_tracker"
-  });
+});
 
-  connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     // run the start function after the connection is made to prompt the user
     start();
-  });
+});
 
-  function start(){
-      inquirer  
+function start() {
+    inquirer
         .prompt({
             name: "MainMenu",
             type: "list",
             message: "What would you like to do?",
             choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View All Roles", "Add Role", "Remove Role", "EXIT"]
         })
-        .then(function(answer){
+        .then(function (answer) {
             const userChoice = answer.MainMenu;
 
             switch (userChoice) {
@@ -67,54 +67,89 @@ var connection = mysql.createConnection({
                     console.log("Goodbye!")
                     connection.end();
                     break;
-                
-                    
+
+
             }
         })
-  };
+};
 
-  function allEmployees(){
-      var query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
-      connection.query(query, function(err, res){
-          if (err) throw err;
-
-          console.table(res);
-          start();
-      })
-
-  };
-
- function addEmployee(){
-
-    connection.query("SELECT * FROM employee WHERE manager_id IS NULL", function(err, results) {
+function allEmployees() {
+    var query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY ID ASC";
+    connection.query(query, function (err, res) {
         if (err) throw err;
 
-        console.table(results);
+        console.table(res);
+        start();
     })
 
-    // inquirer
-    //     .prompt([
+};
 
-    //         {
-    //             name: "firstName",
-    //             type:"input",
-    //             message: "What is the employee's first name?"
-    //         },
+function addEmployee() {
 
-    //         {
-    //             name: "lastName",
-    //             type:"input",
-    //             message: "What is the employee's last name?",
-    //         },
+    
 
-    //         {
-    //             name: "role",
-    //             type: "list",
-    //             message: "What is the employee's role",
-    //             choices: ["Senior Salesperson", "Salesperson", "Accountant", "Software Engineer", "HR Rep", "Junior Engineer", "Senior Engineer", "Junior Accountant", "Senior Accountant", "Senior HR Rep"]
+    // connection.query("SELECT title FROM role", function(err, results) {
+    //     if (err) throw err;
 
-    //         }
-    //     ])
-        
+    //     console.log(results);
+    // });
 
- }
+    roleChoicesArr = [];
+    managerChoicesArr = [];
+
+    connection.query("SELECT title FROM role", function (err, results) {
+        if (err) throw err;
+
+        for (var i = 0; i < results.length; i++) {
+            roleChoicesArr.push(results[i].title)
+        }
+    });
+
+    connection.query("SELECT concat(m.first_name, ' ', m.last_name) AS manager FROM employee m WHERE manager_id IS NULL", function(err, results) {
+        if (err) throw err;
+
+        for (var i = 0; i < results.length; i++) {
+            managerChoicesArr.push(results[i].manager)
+        }
+
+        // console.log(managerChoicesArr);
+
+    });
+
+
+
+    inquirer
+        .prompt([
+
+            {
+                name: "firstName",
+                type: "input",
+                message: "What is the employee's first name?"
+            },
+
+            {
+                name: "lastName",
+                type: "input",
+                message: "What is the employee's last name?",
+            },
+
+            {
+                name: "role",
+                type: "list",
+                message: "What is the employee's role",
+                choices: roleChoicesArr,
+
+            },
+
+            {
+                name: "manager",
+                type: "list",
+                message: "Who is the employee's manager",
+                choices: managerChoicesArr,
+            }
+
+
+        ])
+
+
+}
